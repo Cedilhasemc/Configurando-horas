1ecc00c94c8b350357989fa9af14f04ee212e826
Cedilhasemc <contact.toyku@gmail.com>
2023-05-10 22:55:04 -0300

    finalizando aula

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-//Retornando a Hora
-let horas= new Date()
-document.write(horas.getHours());
-document.write(":")
-
-//Retornando os Minutos
-let minutos = new Date()
-document.write(minutos.getMinutes());
-document.write(":")
-
-//Retornando os Segundos
-let segundos = new Date()
-document.write(segundos.getSeconds());
-document.write(":")
-
-//Retornando os Milissegundos
-let milissegundos = new Date()
-document.write(milissegundos.getMilliseconds());
-document.write("<br><br>")
-
-//Setando a hora
-let horas1 = new Date()
-horas1.setHours(horas1.getHours()+3)
-document.write(horas1.getHours());
-document.write(horas1.toLocaleString());
+let tmp
+function tempo() {
+    let relogio =document.querySelector("#main")
+    let hora = new Date()
+    relogio.innerHTML =(hora.toLocaleString().slice(-8))
+}
+document.querySelector("#ligar").addEventListener('click',()=>{
+    tmp =setInterval(tempo,1000)
+})
+document.querySelector("#desligar").addEventListener('click',()=>{
+    clearInterval(tmp)
+})
